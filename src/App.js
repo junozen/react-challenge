5c43f01d7465e922cf99f9e4e7130e1936e4537f
Jacen Juno <junozens@gmail.com>
2023-03-07 15:51:47 +0200

    initial structuring

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Col, Container, Row } from "react-bootstrap";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="header">
+      <Container>
+        <Row>
+          <Col>
+            <h1 className="text-center mt-3">Hello World</h1>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
